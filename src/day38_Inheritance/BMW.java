f0a8112c4db05833f1602459d3238b1ce254e5e0
makhabatkoshalieva <baiel192017@gmail.com>
2022-01-17 12:50:54 -0500

    Day38-Inheritance-Method Overriding continue

--- src/day38_Inheritance/BMW.java
+++ src/day38_Inheritance/BMW.java
@@ -4,4 +4,12 @@ public class BMW extends Car{
     public BMW(String brand, String model, int year, double price, String color, int miles) {
         super(brand, model, year, price, color, miles);
     }
+
+    public void breaksDown(){
+        System.out.println(brand+" "+model+" is breaks down every 100 miles");
+    }
+
+    public void racing(){
+        System.out.println(brand+" "+model+" is racing");
+    }
 }
--- src/day38_Inheritance/Tesla.java
+++ src/day38_Inheritance/Tesla.java
@@ -4,4 +4,8 @@ public class Tesla extends Car{
     public Tesla(String brand, String model, int year, double price, String color, int miles) {
         super(brand, model, year, price, color, miles);
     }
+    public void autoPilot(){
+        System.out.println(brand+" "+model+" is autopilot mode");
+    }
+
 }
