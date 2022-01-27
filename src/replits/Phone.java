bba64c20b21cd555ab62a9fc0602aaf6ad62ea7f
makhabatkoshalieva <baiel192017@gmail.com>
2022-01-27 09:58:33 -0500

    initial commit

---
+++ src/replits/Phone.java
@@ -0,0 +1,18 @@
+package replits;
+
+public class Phone {
+    static int number;
+
+    public Phone(String str){
+        this(str.length());
+    }
+    public Phone(int num){
+        this();
+        number += num;
+    }
+    public Phone(){
+        number += 1;
+    }
+
+
+}
--- src/replits/Test1.java
+++ src/replits/Test1.java
@@ -1,17 +1,6 @@
 package replits;
 
-public class Test1{
+ class Test1{
 
-    public static void main(String[] args) {
-        method1();
-    }
-    public static void method1() {
-        int n = -5;
-        for (int i = 1; i <5 ; i++) {
-            n *=i;
 
         }
-        System.out.print(n);
-
-    }
-}
